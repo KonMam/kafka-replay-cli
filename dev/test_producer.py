ad9e2a6d54feafeb9b18f3e221bc2a5e7863cf64
Konstantinas Mamonas <k.mamonas@gmail.com>
2025-04-30 18:07:09 +0300

    test producer

---
+++ dev/test_producer.py
@@ -0,0 +1,16 @@
+from confluent_kafka import Producer
+
+p = Producer({"bootstrap.servers": "localhost:9092"})
+
+
+def acked(err, msg):
+    if err:
+        print("Failed to deliver message:", err)
+    else:
+        print(f"Produced to {msg.topic()} [{msg.partition()}] at offset {msg.offset()}")
+
+
+for i in range(5):
+    p.produce("test-topic", key=f"user-{i}", value=f'{{"event":"test-{i}"}}', callback=acked)
+
+p.flush()
